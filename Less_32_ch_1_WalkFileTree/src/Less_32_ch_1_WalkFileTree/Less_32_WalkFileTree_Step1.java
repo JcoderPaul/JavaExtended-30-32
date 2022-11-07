2bbeeda738ca8b400fd04ea3ca6e0b0cedd740e6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2022-11-07 19:59:18 +0300

    Add the Less_32_ch_1_WalkFileTree

---
+++ Less_32_ch_1_WalkFileTree/src/Less_32_ch_1_WalkFileTree/Less_32_WalkFileTree_Step1.java
@@ -0,0 +1,17 @@
+package Less_32_ch_1_WalkFileTree;
+
+import Less_32_ch_1_WalkFileTree.MyClasses.MyFileVisitor;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Less_32_WalkFileTree_Step1 {
+    public static void main(String[] args) throws IOException {
+        Path path_for_scan = Paths.get("E:\\JavaExtended\\Less_32_ch_1_WalkFileTree\\" +
+                                           "src\\Less_32_ch_1_WalkFileTree\\WorkingFolder\\");
+
+        Files.walkFileTree(path_for_scan, new MyFileVisitor());
+    }
+}
